5bed7509cbd40a1483cde50cba51f6cdda7849bc
pushu <1039670791@qq.com>
2019-11-28 17:39:23 +0800

    首次提交

--- app/src/main/java/com/example/androidassistant/MainActivity.java
+++ app/src/main/java/com/example/androidassistant/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,16 +21,43 @@ public class MainActivity extends AppCompatActivity {
 
     final static int REQUECT_CODE_SDCARD = 2;
     TextView mTv;
+    Button mBtn1,mBtn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             mTv = findViewById(R.id.tv);
+            mBtn1 = findViewById(R.id.btn1);
+            mBtn2 = findViewById(R.id.btn2);
+
+
+            mBtn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        mBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Toast.makeText(MainActivity.this,"Text",Toast.LENGTH_SHORT).show();
+            }
+        });
             mTv.setText(VersionUtil.getVersionName(getApplicationContext()));
             mTv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                    // MPermissions.requestPermissions(MainActivity.this, REQUECT_CODE_SDCARD, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+
 
                     XXPermissions.with(MainActivity.this)
                             // 可设置被拒绝后继续申请，直到用户授权或者永久拒绝
